4618b72453c723652d35ab253d05d0f0df46a5cb
Gerardo Gallegos <36527267+gallegosg@users.noreply.github.com>
2024-08-16 17:59:08 -0700

    Initial Commit

---
+++ GetItDone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetItDone
+//
+//  Created by Gerardo Gallegos on 8/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
